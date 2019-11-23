f26150696798609577e3fae899127322bf238ca5
Martin <martin.papa@outlook.com>
2019-11-23 17:43:48 -0500

    cleaned up formatting in menu.js

--- js/menu.js
+++ js/menu.js
@@ -22,26 +22,22 @@ IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused 
 
 IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
 
-
-
 // Function Invocation
 
 displayMenu();
 
 //Global Function
 
-
-
 // Display all the flavor stored in an array to the page
 function displayMenu() {
     
     for(var i = 0; i < IceCream.flavors.length; i++) {
+        
         var li = document.createElement('li');
         var flavorImg = document.createElement('img');
         var flavorName = document.createElement('h4');
         var submitBtn = document.createElement('button');
         submitBtn.id = 'submitBtn';
-        
 
         parentElement.appendChild(li);
         li.appendChild(flavorImg);
@@ -53,13 +49,9 @@ function displayMenu() {
     }
 }
 
-
-
-
 /// Will improve this later, 
 function addToCartBtnHandler() {
     alert("Added to cart");
 }
-
 
 document.getElementById('submitBtn').addEventListener('click', addToCartBtnHandler);
