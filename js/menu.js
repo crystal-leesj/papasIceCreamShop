ccb34edeae1d9f226f68eaa00021aa1bffeef38c
Martin <martin.papa@outlook.com>
2019-11-23 17:32:38 -0500

    added handler for add to cart btn

--- js/menu.js
+++ js/menu.js
@@ -22,13 +22,26 @@ IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused 
 
 IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
 
+
+
+// Function Invocation
+
+displayMenu();
+
+//Global Function
+
+
+
+// Display all the flavor stored in an array to the page
 function displayMenu() {
     
     for(var i = 0; i < IceCream.flavors.length; i++) {
         var li = document.createElement('li');
         var flavorImg = document.createElement('img');
-        var flavorName = document.createElement('p');
+        var flavorName = document.createElement('h4');
         var submitBtn = document.createElement('button');
+        submitBtn.id = 'submitBtn';
+        
 
         parentElement.appendChild(li);
         li.appendChild(flavorImg);
@@ -40,4 +53,13 @@ function displayMenu() {
     }
 }
 
-displayMenu();
+
+
+
+/// Will improve this later, 
+function addToCartBtnHandler() {
+    alert("Added to cart");
+}
+
+
+document.getElementById('submitBtn').addEventListener('click', addToCartBtnHandler);
