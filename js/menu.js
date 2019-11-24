0e264609fdf382dbdf53178c9d1fb252a154972f
Crystal Soo Lee <soo.leesj@gmail.com>
2019-11-23 23:39:20 -0800

    code review martin's code

--- js/menu.js
+++ js/menu.js
@@ -2,25 +2,27 @@
 
 var parentElement = document.getElementById('flavors');
 
-IceCream.flavors = []; // Store all flavors here (**Note** Should I store in Shop.flavor???)
+console.log('***allIceCreams : ', IceCream.allIceCreams);
 
-//// Will change the flavors later according to the image that I will get
+// IceCream.flavors = []; // Store all flavors here (**Note** Should I store in Shop.flavor???)
 
-IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
+// //// Will change the flavors later according to the image that I will get
 
-IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
+// IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
 
-IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
+// IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
 
-IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
+// IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
 
-IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
+// IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
 
-IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
+// IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
 
-IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
+// IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
 
-IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
+// IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
+
+// IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
 
 // Function Invocation
 
@@ -30,28 +32,29 @@ displayMenu();
 
 // Display all the flavor stored in an array to the page
 function displayMenu() {
-    
-    for(var i = 0; i < IceCream.flavors.length; i++) {
-        
-        var li = document.createElement('li');
-        var flavorImg = document.createElement('img');
-        var flavorName = document.createElement('h4');
-        var submitBtn = document.createElement('button');
-        submitBtn.id = 'submitBtn';
+  for(var i = 0; i < IceCream.allIceCreams.length; i++) {
+    var li = document.createElement('li');
+    var flavorImg = document.createElement('img');
+    var flavorName = document.createElement('h4');
+    var flavorDescription = document.createElement('p');
+    var submitBtn = document.createElement('button');
+    submitBtn.id = 'submitBtn';
 
-        parentElement.appendChild(li);
-        li.appendChild(flavorImg);
-        flavorImg.src = IceCream.flavors[i].imgUrl;
-        li.appendChild(flavorName);
-        flavorName.textContent = IceCream.flavors[i].name;
-        submitBtn.innerHTML = "Add To Cart";
-        li.appendChild(submitBtn);
-    }
+    parentElement.appendChild(li);
+    li.appendChild(flavorImg);
+    flavorImg.src = IceCream.allIceCreams[i].imgUrl;
+    li.appendChild(flavorName);
+    flavorName.textContent = IceCream.allIceCreams[i].name;
+    submitBtn.innerHTML = 'Add To Cart';
+    li.appendChild(submitBtn);
+    li.appendChild(flavorDescription);
+    flavorDescription.textContent = IceCream.allIceCreams[i].description;
+  }
 }
 
-/// Will improve this later, 
+/// Will improve this later
 function addToCartBtnHandler() {
-    alert("Added to cart");
+  alert('Added to cart');
 }
 
 document.getElementById('submitBtn').addEventListener('click', addToCartBtnHandler);
