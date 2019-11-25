23f8c537f833f5ad876959ad929ff94f31b37834
Crystal Soo Lee <soo.leesj@gmail.com>
2019-11-25 14:58:48 -0800

    fixed the indentation

--- js/home.js
+++ js/home.js
@@ -3,14 +3,14 @@
 console.log(IceCream.allIceCreams);
 
 function getImage (){
-    var random = Math.floor(Math.random() * IceCream.allIceCreams.length);
-    var img = IceCream.allIceCreams[random].imgUrl;
-    return img;
+  var random = Math.floor(Math.random() * IceCream.allIceCreams.length);
+  var img = IceCream.allIceCreams[random].imgUrl;
+  return img;
 }
 function renderImage () {
-    var imgElem = document.getElementById("random_image");
-    imgElem.setAttribute('src', getImage())
+  var imgElem = document.getElementById("random_image");
+  imgElem.setAttribute('src', getImage());
 }
 renderImage();
 
-setInterval(renderImage,3000);
+setInterval(renderImage, 3000);
