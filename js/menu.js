ce3e802f216cf6cd3171b71370836e30597d1495
Martin Papa <martin.papa@outlook.com>
2019-11-25 12:14:55 -0800

    fixed the layout, special flavors is now added to the page with pictures

--- js/menu.js
+++ js/menu.js
@@ -2,28 +2,6 @@
 
 var parentElement = document.getElementById('flavors');
 
-// console.log('***allIceCreams : ', IceCream.allIceCreams);
-
-// IceCream.flavors = []; // Store all flavors here (**Note** Should I store in Shop.flavor???)
-
-// //// Will change the flavors later according to the image that I will get
-
-// IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
-
-// IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
-
-// IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
-
-// IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
-
-// IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
-
-// IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
-
-// IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
-
-// IceCream.flavors.push(new IceCream('Flavor', 'Organic vanilla ice cream infused with hot fudge and huge chunk of brownie pieces', 'Vanilla Ice Cream, Fudge, Brownies', false, 8.99, 'https://place-hold.it/300x300/#00000/#fffff'));
-
 // Function Invocation
 
 displayMenu();
@@ -35,22 +13,25 @@ function displayMenu() {
   for(var i = 0; i < IceCream.allIceCreams.length; i++) {
     var li = document.createElement('li');
     var flavorImg = document.createElement('img');
+    var flavorDescription = document.createElement('p');
     var flavorName = document.createElement('h4');
-    // var flavorDescription = document.createElement('p');
-    var submitBtn = document.createElement('button');
+    var submitBtn = document.createElement('INPUT');
+    submitBtn.setAttribute('type', 'submit');
+    submitBtn.setAttribute('value', 'Add To Cart');
     submitBtn.id = 'submitBtn';
 
     parentElement.appendChild(li);
     li.appendChild(flavorImg);
+    li.appendChild(flavorDescription);
     flavorImg.src = IceCream.allIceCreams[i].imgUrl;
     li.appendChild(flavorName);
     flavorName.textContent = IceCream.allIceCreams[i].name;
-    submitBtn.innerHTML = 'Add To Cart';
     li.appendChild(submitBtn);
     // li.appendChild(flavorDescription);
     // flavorDescription.textContent = IceCream.allIceCreams[i].description;
   }
 }
+
 
 /// Will improve this later
 function addToCartBtnHandler() {
