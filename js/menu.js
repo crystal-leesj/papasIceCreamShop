fec8c4349eb6d84297ed76da5cd17b012dd1ce25
Crystal Soo Lee <soo.leesj@gmail.com>
2019-11-25 14:53:01 -0800

    initial commit for cart functionality

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var parentElement = document.getElementById('flavors');
+var addToCartBtns = document.getElementsByClassName('addToCartBtn');
+var checkoutBtn = document.getElementById('checkout');
 
 // Function Invocation
 
@@ -19,7 +21,7 @@ function displayMenu() {
     addToCartBtn.setAttribute('type', 'submit');
     addToCartBtn.setAttribute('value', 'Add To Cart');
     addToCartBtn.className = 'addToCartBtn';
-    addToCartBtn.id = `${IceCream.allIceCreams[i].name}'`;
+    addToCartBtn.id = `${IceCream.allIceCreams[i].name}`;
 
 
     parentElement.appendChild(li);
@@ -34,20 +36,27 @@ function displayMenu() {
     // flavorDescription.textContent = IceCream.allIceCreams[i].description;
   }
 
-  
 }
 
-function addToCartBtnHandler() {
+var cart = [];
+
+function addToCartBtnHandler(event) {
   alert('Added to cart:');
   console.log('event.target:', event.target);
+  cart.push(event.target.id);
+  console.log('cart :', cart);
 }
 
-var addToCartBtns = document.getElementsByClassName('addToCartBtn');
-
+// ref: https://stackoverflow.com/questions/19655189/javascript-click-event-listener-on-class
 for(var i = 0; i < addToCartBtns.length; i++) {
   addToCartBtns[i].addEventListener('click', addToCartBtnHandler, false);
 }
 
+function checkoutHandler(event) {
+  alert('Checkout!');
+  console.log('event.target:', event.target);
+}
 
+checkoutBtn.addEventListener('click', checkoutHandler);
 
 
