ff227f46deda7763ff1d40cbdd3fa4afa5c628ff
Martin Papa <martin.papa@outlook.com>
2019-11-26 13:09:23 -0800

    hover is finalyl working, displaing in the center of image

--- js/menu.js
+++ js/menu.js
@@ -17,10 +17,12 @@ function displayMenu() {
     var flavorDescription = document.createElement('p');
     var flavorName = document.createElement('h4');
     var addToCartBtn = document.createElement('INPUT');
-    imageDiv.id = 'imageDiv' + i;
-    flavorImg.id = 'imageID'
+    // imageDiv.id = 'imageDiv' + i;
+    imageDiv.className = 'divClass';
+    flavorImg.id = 'imageID' + i;
     flavorImg.className = 'image';
     flavorDescription.id = 'description' + i;
+    flavorDescription.className = 'descriptionClass';
     addToCartBtn.setAttribute('type', 'submit');
     addToCartBtn.setAttribute('value', 'Add To Cart');
     addToCartBtn.className = 'addToCartBtn';
@@ -47,7 +49,7 @@ function mouseOverHandler(e) {
   var iceCreamDescriptionNum = e.target.id.charAt(e.target.id.length - 1);
   var descriptionId = 'description' + iceCreamDescriptionNum;
   var descriptionElem = document.getElementById(descriptionId);
-  // console.log(descriptionId);
+  console.log(descriptionId);
   if(descriptionElem)
   {
     descriptionElem.setAttribute('style', 'display: block;');
@@ -67,10 +69,16 @@ function mouseOutHandler(e) {
   }
 }
 
+var arrayOfImages = document.getElementsByClassName('image');
 
-document.getElementById('flavors').addEventListener('mouseover', mouseOverHandler);
-document.getElementById('flavors').addEventListener('mouseout', mouseOutHandler);
+for(var i = 0; i < arrayOfImages.length; i++){
+  arrayOfImages[i].addEventListener('mouseover', mouseOverHandler);
+  arrayOfImages[i].addEventListener('mouseout', mouseOutHandler);
 
+}
+
+// document.getElementsByClassName('image').addEventListener('mouseover', mouseOverHandler);
+// document.getElementsByClassName('image').addEventListener('mouseout', mouseOutHandler);
 
 
 function addToCartBtnHandler() {
