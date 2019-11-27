662bc52ab08c4d2e4867d099539ee44ec1033095
Zhang <zyihang@38f9d3618f7e.ant.amazon.com>
2019-11-26 16:49:59 -0800

    sales form done

---
+++ js/chart.js
@@ -0,0 +1,42 @@
+// 'use strict'
+// // import * shoppingCart;
+// function myChart(){
+//     var ctx = document.getElementById("myChart");
+//     var ctx = document.getElementById("myChart").getContext('2d');
+//     var myChart = new Chart(ctx,{
+//         type:'bar',
+//         data:{
+//             labels:['se','ree','uyt', 'uyt'],
+//             datasets:[{
+//                 label:"number of clicks", //DANGER NEED TO FIX
+//                 data: [1,2,3,4],
+//                 backgroundColor: [
+//                     'blue',
+//                     'blue',
+//                     'blue',
+//                     'blue'
+
+//                 ],
+//                 borderColor:[
+//                     'white',
+//                     'white',
+//                     'white',
+//                     'white'
+//             ],
+//             borderWidth:1
+//             }]
+
+    
+//         },
+//         options:{
+//             scales:{
+//                 yAxes:[{
+//                     ticks:{
+//                         beginAzero:false
+//                     }
+//                 }]
+//             }
+//         }
+//     });
+// }
+// myChart();
